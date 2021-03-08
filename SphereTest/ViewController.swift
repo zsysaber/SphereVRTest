cdf489ce4365ea03dcf75d26a58d190a9afdb869
zsysaber <zsysaber@yahoo.co.jp>
2021-03-08 17:18:07 +0900

    尝试了网上的video和img素材，改正陀螺仪的方向

--- SphereTest/ViewController.swift
+++ SphereTest/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
+import AVFoundation
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scnView: SCNView!
+    let cameraNode = SCNNode()
+    let motionManager = CMMotionManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        
+        scnView.showsStatistics = true
+        //可以旋转模型
+        //scnView.allowsCameraControl = true
+        let scene = SCNScene()
+        scnView.scene = scene
+   
+        cameraNode.position = SCNVector3(0, 0, 0)
+        cameraNode.camera = SCNCamera()
+        
+       
+        
+        let sphere = SCNSphere(radius: 50)
+        
+        sphere.firstMaterial?.cullMode = .front
+        sphere.firstMaterial?.isDoubleSided = false
+        
+//        let url = Bundle.main.url(forResource: "img", withExtension: "jpg")
+//        sphere.firstMaterial?.diffuse.contents = url
+        sphere.firstMaterial?.diffuse.contents = play()
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0, 0, 0)
+    
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(sphereNode)
+        //scnView.play(self)
+        guard motionManager.isDeviceMotionAvailable else {
+            fatalError("Device motion is not available")
+        }
+        addMotion()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.scnView.play(self)
+    }
+    func play() -> SKScene{
+        guard let url = Bundle.main.url(forResource: "b", withExtension: "mp4") else{
+            fatalError()
+        }
+        print("aaaaa")
+        let player = AVPlayer(url: url)
+        let videoNode = SKVideoNode(avPlayer: player)
+        videoNode.size = CGSize(width: 2000, height: 1000)
+        //videoNode.size = CGSize(width:view.frame.width, height:view.frame.height)
+        videoNode.position = CGPoint(x: videoNode.size.width/2, y: videoNode.size.height/2)
+        videoNode.zRotation = CGFloat(Double.pi)
+        videoNode.play()
+        let skScene = SKScene(size: videoNode.size)
+        skScene.addChild(videoNode)
+        return skScene
+    }
+    //调用coremotion
+    func addMotion(){
+        motionManager.deviceMotionUpdateInterval = 1.0/60.0
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main) {
+            [weak self](data: CMDeviceMotion?, error: Error?) in
+            guard let data = data else { return }
+            let attitude: CMAttitude = data.attitude
+            self?.cameraNode.eulerAngles = SCNVector3Make(-Float(attitude.roll + Double.pi/2), Float(attitude.yaw), -Float(attitude.pitch))
+        }
+    }
 
 }
 
